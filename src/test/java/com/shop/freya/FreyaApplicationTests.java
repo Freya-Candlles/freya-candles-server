dd71b6bc32437f01aeae2564a653ea0a56d00d7a
Stepan Omeliukh <72635526+StepanOmeliukh@users.noreply.github.com>
2022-03-26 23:33:41 +0200

    Initial commit. Version 0.0.0

---
+++ src/test/java/com/shop/freya/FreyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.freya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreyaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
